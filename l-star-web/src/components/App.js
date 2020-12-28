0ff4f6a40da40bea29f3fea6cb788622d26f5acb
Jon Lam <jonpawlam@gmail.com>
2020-12-28 12:44:58 +0100

    test 2

--- l-star-web/src/components/App.js
+++ l-star-web/src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import Header from "./Header";
+<<<<<<< HEAD
 import firebase from "../api/firebase";
 import OpslagsAPI from "../api/OpslagAPI";
+import Footer from "./Footer"
+>>>>>>> 19eac9a8a3530d158576a82e014e959a2c7ab809
 
 class App extends Component {
   render() {
@@ -15,6 +18,9 @@ class App extends Component {
            
           </div>
         </header>
+        <footer className="App-footer">
+          <Footer />
+        </footer>
       </div>
     );
   }
