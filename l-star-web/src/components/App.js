e107e0e835e0ce1e3448136ffad1e20be403850a
Jon Lam <jonpawlam@gmail.com>
2020-12-28 12:51:36 +0100

    Master branch Bridge
    Lad os branche her fra

--- l-star-web/src/components/App.js
+++ l-star-web/src/components/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import Header from "./Header";
-<<<<<<< HEAD
-import firebase from "../api/firebase";
 import OpslagsAPI from "../api/OpslagAPI";
 import Footer from "./Footer"
->>>>>>> 19eac9a8a3530d158576a82e014e959a2c7ab809
+
 
 class App extends Component {
   render() {
